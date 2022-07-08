f542909fdfacb98c1afd415236a897794c0704c1
wdey <wdey@cozeva.com>
2022-07-08 14:25:47 +0530

    Testing commit and pull

--- main.py
+++ main.py
@@ -16,10 +16,6 @@ if __name__ == '__main__':
         exit(9)
     if guiwindow.verification_specs[4][13] == 1:
         import runner
-        #hello world
-        #Adding this statement from github
-        #This statement has been added from pycharm
-        #This statement is now a pull Request
 
 
         exit(45)
