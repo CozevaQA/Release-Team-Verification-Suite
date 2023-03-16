88e8bf37ee7ed36a6fa7f01f0f7d10284f0b6126
wdey <wdey@cozeva.com>
2023-03-16 18:07:12 +0530

    stage Verification

--- RTVS_Master.py
+++ RTVS_Master.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from PIL import ImageTk, Image
 
+# stage Branch
 def master_gui():
     root = Tk()
 
     def on_nextbutton():
-        x=0
+        x = 0
 
-    #Widgets
+    # Widgets
     please_select_label = Label(root, text="Select Script", font=("Nunito Sans", 10))
     # script_var = IntVar()
     # daily_validation_radio = Radiobutton(root, text="Verification Suite", variable=script_var, value=0, font=("Nunito Sans", 10))
@@ -19,34 +20,42 @@ def master_gui():
     #
     # #load widgets onto root
     please_select_label.grid(row=0, column=1, columnspan=5, sticky="w")
+
     # daily_validation_radio.grid(row=1, column=1, columnspan=5, sticky="w")
     # hcc_validation_radio.grid(row=2, column=1, columnspan=5, sticky="w")
     # global_search_radio.grid(row=3, column=1, columnspan=5, sticky="w")
     # next_button.grid(row=4, column=1, columnspan=5)
 
-    #TRYING SOMETHING ELSE, HOPING THIS WORKS ITS 3 AM
+    # TRYING SOMETHING ELSE, HOPING THIS WORKS ITS 3 AM
 
     def on_first_time_setup():
         root.destroy()
         import FirstTimeSetup
+
     def on_verification_suite():
         root.destroy()
         import main
+
     def on_hcc_validation():
         root.destroy()
         import HCC_Validation_multi
+
     def on_global_search():
         root.destroy()
         import global_search
+
     def on_task_ingestion():
         root.destroy()
         import ProspectInjestHCC
+
     def on_analytics():
         root.destroy()
         import runner
+
     def on_slow_trends():
         root.destroy()
         import slowLogPlotter
+
     def on_role_access():
         root.destroy()
 
@@ -62,7 +71,8 @@ def master_gui():
         image_small = Image.open(image_path).resize((25, 25))
 
         return image_small
-    #making image widgets
+
+    # making image widgets
     first_time_setup_image = ImageTk.PhotoImage(image_sizer("assets/images/first_time_setup.png"))
     verification_suite_image = ImageTk.PhotoImage(image_sizer("assets/images/verification_suite.png"))
     hcc_validation_image = ImageTk.PhotoImage(image_sizer("assets/images/hcc_validation.png"))
@@ -78,19 +88,24 @@ def master_gui():
                                                                                                       column=1,
                                                                                                       columnspan=4,
                                                                                                       sticky="w")
-    Button(root, text="Verification Suite", command=on_verification_suite, font=("Nunito Sans", 10)).grid(row=2, column=1, columnspan=4, sticky="w")
-    Button(root, text="HCC Validation Multi (Custom client list)", command=on_hcc_validation, font=("Nunito Sans", 10)).grid(row=3,
-                                                                                                        column=1,
-                                                                                                        columnspan=4,
-                                                                                                        sticky="w")
+    Button(root, text="Verification Suite", command=on_verification_suite, font=("Nunito Sans", 10)).grid(row=2,
+                                                                                                          column=1,
+                                                                                                          columnspan=4,
+                                                                                                          sticky="w")
+    Button(root, text="HCC Validation Multi (Custom client list)", command=on_hcc_validation,
+           font=("Nunito Sans", 10)).grid(row=3,
+                                          column=1,
+                                          columnspan=4,
+                                          sticky="w")
     Button(root, text="Global Search", command=on_global_search, font=("Nunito Sans", 10)).grid(row=4,
                                                                                                 column=1,
                                                                                                 columnspan=4,
                                                                                                 sticky="w")
-    Button(root, text="Task Ingestion (AWV Only, Others WIP)", command=on_task_ingestion, font=("Nunito Sans", 10)).grid(row=5,
-                                                                                                                        column=1,
-                                                                                                                        columnspan=4,
-                                                                                                                        sticky="w")
+    Button(root, text="Task Ingestion (AWV Only, Others WIP)", command=on_task_ingestion,
+           font=("Nunito Sans", 10)).grid(row=5,
+                                          column=1,
+                                          columnspan=4,
+                                          sticky="w")
     Button(root, text="Analytics (Default Config Schema)", command=on_analytics, font=("Nunito Sans", 10)).grid(row=6,
                                                                                                                 column=1,
                                                                                                                 columnspan=4,
@@ -99,15 +114,18 @@ def master_gui():
                                                                                                 column=1,
                                                                                                 columnspan=4,
                                                                                                 sticky="w")
-    Button(root, text="Multi-role Access Check", command=on_role_access, font=("Nunito Sans", 10)).grid(row=8, column=1, columnspan=5, sticky="w")
+    Button(root, text="Multi-role Access Check", command=on_role_access, font=("Nunito Sans", 10)).grid(row=8, column=1,
+                                                                                                        columnspan=5,
+                                                                                                        sticky="w")
     Button(root, text="Special Columns", command=on_special_columns, font=("Nunito Sans", 10)).grid(row=9,
-                                                                                                column=1,
-                                                                                                columnspan=4,
-                                                                                                sticky="w")
-    Button(root, text="Hospital Activity (All Clients)", command=on_hospital_activity, font=("Nunito Sans", 10)).grid(row=10,
                                                                                                     column=1,
                                                                                                     columnspan=4,
                                                                                                     sticky="w")
+    Button(root, text="Hospital Activity (All Clients)", command=on_hospital_activity, font=("Nunito Sans", 10)).grid(
+        row=10,
+        column=1,
+        columnspan=4,
+        sticky="w")
     Label(root, image=first_time_setup_image, width=40, height=40).grid(row=1, column=0, sticky="w")
     Label(root, image=verification_suite_image, width=40, height=40).grid(row=2, column=0, sticky="w")
     Label(root, image=hcc_validation_image, width=40, height=40).grid(row=3, column=0, sticky="w")
@@ -118,13 +136,6 @@ def master_gui():
     Label(root, image=multi_role_image, width=40, height=40).grid(row=8, column=0, sticky="w")
     Label(root, image=special_column_image, width=40, height=40).grid(row=9, column=0, sticky="w")
     Label(root, image=hospital_activity_image, width=40, height=40).grid(row=10, column=0, sticky="w")
-
-
-
-
-
-
-
 
     root.title("Release Team Master Suite")
     root.iconbitmap("assets/icon.ico")
