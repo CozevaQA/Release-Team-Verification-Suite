ef00a0e0f311427afb1f7947d5637b11d86a6be4
wdey <wdey@cozeva.com>
2023-03-30 21:43:00 +0530

    Exe update

--- update_RTVS.py
+++ update_RTVS.py
@@ -5,6 +5,7 @@ import subprocess
 import os
 
 cwd = os.getcwd()
+subprocess.run(["git", "stash"], check=True, cwd=cwd, shell=True)
 subprocess.run(["git", "pull"], check=True, cwd=cwd, shell=True)
 
 
