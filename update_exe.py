273be4821ce2202ad0df4b21da35e5203d8f9cfb
wdey <wdey@cozeva.com>
2023-03-30 20:59:46 +0530

    Exe update

--- update_exe.py
+++ update_exe.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
 
-# Get the current working directory
-cwd = os.getcwd()
+import subprocess
+import os
 
-# Open a new terminal console and run the git pull command
-subprocess.Popen(["gnome-terminal", "--", "bash", "-c", f"cd {cwd} && git pull; exec bash"], stdin=subprocess.PIPE)
+cwd = os.getcwd()
+subprocess.run(["git", "pull"], check=True, cwd=cwd, shell=True)
+
 
